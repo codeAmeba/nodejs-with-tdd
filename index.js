03c12b5cd0c0c001cbc6b550e4e75f45a1c63535
jeongsooyoung <code.ameba@gmail.com>
2020-10-17 17:39:20 +0900

    get users api test

--- index.js
+++ index.js
@@ -11,8 +11,18 @@ const users = [
 app.use(morgan('dev'));
 
 app.get('/users', (req, res) => {
-  const limit = req.query.limit;
+  req.query.limit = req.query.limit || 10;
+  const limit = parseInt(req.query.limit, 10);
+  if (Number.isNaN(limit)) {
+    return res.status(400).end();
+  }
   res.json(users.slice(0, limit));
+});
+
+app.get('/users/:id', function (req, res) {
+  const id = parseInt(req.params.id, 10);
+  const user = users.filter(user => user.id === id)[0];
+  res.json(user);
 });
 
 app.listen(3000, () => {
--- test/index.spec.js
+++ test/index.spec.js
@@ -22,4 +22,28 @@ describe('GET /users is', () => {
         });
     });
   });
+
+  describe('실패시', () => {
+    it('limit이 숫자형이 아니면 400 응답', done => {
+      request(app)
+        .get('users?limit=two')
+        .expect(400)
+        .end((err, res) => {
+          done();
+        });
+    });
+  });
+});
+
+describe('GET /users/1 is...', () => {
+  describe('성공시', () => {
+    it('id가 1인 유저 객체 반환', done => {
+      request(app)
+        .get('/users/1')
+        .end((err, res) => {
+          res.body.should.have.property('id', 1);
+          done();
+        });
+    });
+  });
 });
